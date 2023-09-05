3d1b2efe3200e820bf1bddb187ca4341c07b9b3e
beingamit4197 <beingamit4197@gmail.com>
2023-09-05 13:12:43 +0530

    Nested Routes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-export const Navbar = () => {
-    const navLinkStyle = ({ isActive }) => {
-        return {
-            fontWeight: isActive ? 'bold' : 'normal',
-            textDecoration: isActive ? 'none' : 'underline',
-        }
+export const navLinkStyle = ({ isActive }) => {
+    return {
+        fontWeight: isActive ? 'bold' : 'normal',
+        textDecoration: isActive ? 'none' : 'underline',
     }
+}
+
+export const Navbar = () => {   
   return (
-    <nav>
+    <nav className='primary-nav'>
         <NavLink style={navLinkStyle} to='/'>Home</NavLink>
         <NavLink style={navLinkStyle} to='/about'>About</NavLink>
+        <NavLink style={navLinkStyle} to='/products'>Products</NavLink>
     </nav>
   )
 }
