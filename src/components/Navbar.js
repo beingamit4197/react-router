427e870c5f2fe82fbd78b21f01145397a36a568b
beingamit4197 <beingamit4197@gmail.com>
2023-09-05 12:09:16 +0530

    Navigating Programmatically

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+export const Navbar = () => {
+    const navLinkStyle = ({ isActive }) => {
+        return {
+            fontWeight: isActive ? 'bold' : 'normal',
+            textDecoration: isActive ? 'none' : 'underline',
+        }
+    }
+  return (
+    <nav>
+        <NavLink style={navLinkStyle} to='/'>Home</NavLink>
+        <NavLink style={navLinkStyle} to='/about'>About</NavLink>
+    </nav>
+  )
+}
